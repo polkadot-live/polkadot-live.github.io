35029c7e38e0b8ce5f08dd70ebce06b41c4c1a56
Dane Bulat <dane.bulat@gmail.com>
2024-06-27 13:35:29 +0100

    docs: subscriptions docs foundation (#6)

--- src/components/Img/index.tsx
+++ src/components/Img/index.tsx
@@ -1,10 +1,15 @@
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
-export const Img = ({url}) => (
+interface ImgProps {
+  url: string;
+  maxHeight?: string;
+}
+
+export const Img = ({url, maxHeight = '400px'}: ImgProps) => (
   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '1em 0' }}>
     <img
       src={useBaseUrl(url)}
-      style={{ maxHeight: '400px' }}
+      style={{ maxHeight }}
     />
   </div>
 );
